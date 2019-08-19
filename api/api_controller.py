f83c7f4d78544c1f1fd1ebbed0160f9ba5fc81b9
RuchiJuneja1 <ruchi.juneja30@gmail.com>
2019-08-19 14:22:37 +0530

    updated model with api code changes

--- api/api_controller.py
+++ api/api_controller.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask import request, jsonify, make_response
+from flask import Flask
+from flask import request, make_response
 from sklearn.externals import joblib
 import pandas as pd
 
@@ -7,27 +7,24 @@ import pandas as pd
 import predict_main
 import os
 
-import io
-import csv
-
 app = Flask(__name__)
 
 
+# load model file and scalar file
 current_dir = os.path.dirname(__file__)
-# dataset_file = os.path.join(current_dir, "../data/dataset.csv")
-# data_file = pd.read_csv(dataset_file, sep=";")
 model_file = os.path.join(current_dir, "../data/prediction_model.sav")
 loaded_model = joblib.load(model_file)
 model_scaler_file = os.path.join(current_dir, "../data/scaler.sav")
 model_scaler = joblib.load(model_scaler_file)
 
 
+# api entry point: generates a HTML form to upload dataset file
 @app.route('/')
 def form():
     return """
         <html>
             <body>
-                <h1>Transform a file demo</h1>
+                <h1>Upload dataset file to get predictions</h1>
 
                 <form action="/predict" method="post" enctype="multipart/form-data">
                     <input type="file" name="data_file" />
@@ -39,20 +36,33 @@ def form():
 
 
 
-@app.route('/predict', methods=["POST"])
-def predict_api():
-	dataset = pd.read_csv(request.files.get('data_file'), sep=';')
-	default_prediction_df = predict_main.predict_default(loaded_model, dataset, model_scaler)
-	resp = make_response(default_prediction_df.to_csv())
+def send_response(dataframe):
+	'''
+
+	:param dataframe: resulting dataframe to be returned via CSV
+	:return: csv with <uuid>;<pd> or error csv with error message
+	'''
+	resp = make_response(dataframe.to_csv())
 	resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
 	resp.headers["Content-Type"] = "text/csv"
 	return resp
 
 
-# @app.route('/', methods=['GET'])
-# def predict_api():
-# 	obj = predict_main.predict_default(loaded_model, data_file, model_scaler)
-# 	return jsonify(obj)
+@app.route('/predict', methods=["POST"])
+def predict_api():
+	'''
+
+	:return: csv file for the records where default is null. CSV format: <uuid>;<pd> or error csv with error message
+	'''
+	try:
+		dataset = pd.read_csv(request.files.get('data_file'), sep=';')
+	except pd.io.common.EmptyDataError:
+		error_df = pd.DataFrame(columns=['error_message'])
+		error_df.loc[0] = "Blank file uploaded"
+		return send_response(error_df)
+	default_prediction_df = predict_main.predict_default(loaded_model, dataset, model_scaler)
+	return send_response(default_prediction_df)
+
 
 
 if __name__ == '__main__':
--- api/predict_main.py
+++ api/predict_main.py
@@ -6,52 +6,108 @@ sys.path.insert(1,"common")
 from constants import *
 
 
-def predict_default(loaded_model, data_file, model_scaler):
-    aggregated_column_list = []
-    new_cat_names = []
-    aggregated_numerical_features = []
+def send_error_file(error_message):
+	'''
 
-    non_default = data_file.loc[data_file['default'].isnull()]
-    print non_default.shape
-    non_default.drop(columns=dropped_columns_due_to_missing_data, axis=1, inplace=True)
+	:param error_message: error message to be embedded in response csv file
+	:return: error dataframe
+	'''
+	error_df = pd.DataFrame(columns=['error_message'])
+	error_df.loc[0] = error_message
+	return error_df
+
+
+def validate_input(data_file):
+	'''
+
+	:param data_file: user input data file
+	:return: error dataframe if validation fails, "validated" otherwise
+	'''
+	input_file_column_names = data_file.columns
+	if set(input_file_column_names) != set(required_input_file_parameter):
+		error_message = "parameters name in given input file does not match the requirement."
+		return send_error_file(error_message)
+
+	if len(data_file.loc[data_file['default'].isnull()])==0:
+		error_message = "input file does not contain records where default is not defined."
+		return send_error_file(error_message)
+
+	return "validated"
+
+
+def features_aggregation(null_default):
+    '''
+
+    :param null_default: pandas dataframe where default is not defined
+    :return: altered dataframe with new aggregated fields added
+    '''
+    aggregated_numerical_features =[]
+    aggregated_column_list = []
     for key in aggregation_dict:
-        # print aggregation_dict[key]
         aggregated_numerical_features.append(key)
         aggregator = 0
         temp_df = pd.DataFrame()
         for col,factor in aggregation_dict[key].items():
             aggregator += factor
             aggregated_column_list.append(col)
-            temp_df = pd.concat([temp_df, factor*non_default[col]], axis=1)
-        non_default[key] = pd.Series(temp_df.sum(axis=1)/aggregator)
-    print non_default.shape
-    non_default.drop(columns=aggregated_column_list, axis=1, inplace=True)
-    print non_default.columns
-    print non_default.shape
-    numX = [col for col in numerical_parameters if col in non_default.columns]
-    numX += aggregated_numerical_features
-    print "***********************"
-    print numX
-    numericX = non_default[numX]
+            temp_df = pd.concat([temp_df, factor*null_default[col]], axis=1)
+        null_default[key] = pd.Series(temp_df.sum(axis=1)/aggregator)
 
-    print numericX.columns
+    null_default.drop(columns=aggregated_column_list, axis=1, inplace=True)
+    return (null_default, aggregated_numerical_features)
+
+
+def predict_default(loaded_model, data_file, model_scaler):
+    '''
+
+    :param loaded_model: pre-trained prediction model file
+    :param data_file: user uploaded dataset file
+    :param model_scaler: scaler used in the model to scale numeric data
+    :return: csv file for the records where default is null. CSV format: <uuid>;<pd>
+    '''
+
+    new_cat_names = []
+
+    input_file_validation_status = validate_input(data_file)
+    if type(input_file_validation_status)!=str:
+		return input_file_validation_status
+
+    # extract records where default is not defined
+    null_default = data_file.loc[data_file['default'].isnull()]
+
+    # remove columns that are not required for prediction
+    redundant_fields = dropped_columns_due_to_missing_data + correlated_drop_feature_list
+    null_default.drop(columns=redundant_fields, axis=1, inplace=True)
+
+    # create new aggregated columns
+    null_default_df, aggregated_numerical_features = features_aggregation(null_default)
+    numX = [col for col in numerical_parameters if col in null_default_df.columns]
+    numX += aggregated_numerical_features
+    numericX = null_default_df[numX]
     scaled_numericX = pd.DataFrame(model_scaler.transform(numericX), columns=numericX.columns)
 
+    # create dummy variables for categorical data
     catX = pd.DataFrame()
-    categorical_features = [feature for feature in categorical_parameters if feature not in dropped_columns_due_to_missing_data]
+    categorical_features = [feature for feature in categorical_parameters if feature not in redundant_fields]
     for col in categorical_features:
         cat_col = col + "_cat"
         new_cat_names.append(cat_col)
-        cat_col = pd.get_dummies(pd.Categorical(pd.Series(non_default[col])), prefix=col, drop_first=True)
+        cat_col = pd.get_dummies(pd.Categorical(pd.Series(null_default_df[col])), prefix=col, drop_first=True)
         catX = pd.concat([catX, cat_col], axis=1)
 
+    # collate numerical and categorical features
+    # remove all features not required for prediction post feature encoding
     final_data = pd.concat([scaled_numericX, catX], axis=1)
-    print "final_data : ", final_data.shape
     pmi_drop_column_list_for_input_data = [col for col in final_data.columns if col in pmi_drop_column_list]
-    final_data.drop(list(pmi_drop_column_list_for_input_data), axis=1, inplace=True)
-    print final_data.columns
-    y_pred = loaded_model.predict(final_data)
-    y_pred = pd.DataFrame(pd.Series(np.where(y_pred==-1, 0, y_pred)))
+    low_relevant_features = [col for col in final_data.columns if col in non_important_features]
+    features_drop_list = pmi_drop_column_list_for_input_data + low_relevant_features
+    final_data.drop(list(features_drop_list), axis=1, inplace=True)
 
-    output_df = pd.concat([non_default['uuid'], y_pred], axis=1)
+    ordered_final_data = final_data[model_column_list]
+    default_pred = loaded_model.predict_proba(pd.DataFrame(ordered_final_data))
+    default_pred = np.around(default_pred, decimals=2)
+    default_pred = pd.DataFrame(pd.Series([default_1 for default_0, default_1 in default_pred]))
+
+    output_df = pd.concat([pd.DataFrame(null_default_df['uuid']).set_index(default_pred.index), default_pred], axis=1)
+    output_df.columns = ['uuid', 'pd']
     return output_df
